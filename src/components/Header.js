4daec192ae8aa2369d31b742f698a2b0b7541851
KumaravelThondaman <95341923+KumaravelThondaman@users.noreply.github.com>
2024-05-04 20:21:04 +0530

    changes inprogress

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,61 +1,71 @@
 import React, { useState } from "react";
-import { Bars3Icon } from '@heroicons/react/24/solid'
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
-export default function Header(){
-    const headerList = [
-        {
-            id:1,
-            name: "Home",
-            navPlace:"#"
-        },
-        {
-            id:2,
-            name: "About",
-            navPlace:"#about"
-        },
-        {
-            id:3,
-            name: "Projects",
-            navPlace:"#project"
-        },
-        {
-            id:4,
-            name: "Resume",
-            navPlace:"#resume"
-        },
-        {
-            id:5,
-            name: "Contact",
-            navPlace: "#contact"
-        }
-    ];
+export default function Header() {
+  const headerList = [
+    {
+      id: 1,
+      name: "Home",
+      navPlace: "#",
+    },
+    {
+      id: 2,
+      name: "About",
+      navPlace: "#about",
+    },
+    {
+      id: 3,
+      name: "Projects",
+      navPlace: "#project",
+    },
+    {
+      id: 4,
+      name: "Resume",
+      navPlace: "#resume",
+    },
+    {
+      id: 5,
+      name: "Contact",
+      navPlace: "#contact",
+    },
+  ];
 
-    const [toggleNav, setToggleNav] = useState(false);
-    return(
-        <header className="flex justify-between px-10" id="header">
-            <div className="flex">
-                <div className="text-green-300 text-4xl font-bold">Kumaravel Thondaman</div>
-            </div>
-            <div className="hidden md:block">
-                <ul className="flex flex-end gap-10">
-                    {headerList.map((item) => (
-                        <li className="header-li" key={item.id}><a href={item.navPlace}>{item.name}</a></li>
-                    ))}
-                </ul>
-            </div>
-            { toggleNav &&
-                            <div className="block md:hidden mobile-nav">
-                            <ul className="flex flex-col">
-                                {headerList.map((item) => (
-                                    <li className="border-nav" key={item.id}>{item.name}</li>
-                                ))}
-                            </ul>
-                        </div>
-            }
+  const [toggleNav, setToggleNav] = useState(false);
+  return (
+    <header
+      className="flex justify-between px-10 w-full text-center shadow-2xl bg-gray-800 p-4 shadow-md sticky top-0 z-10"
+      id="header"
+    >
+      <div className="flex">
+        <div className="text-white text-4xl font-bold">Kumaravel T</div>
+      </div>
+      <div className="hidden md:block">
+        <ul className="flex flex-end gap-10">
+          {headerList.map((item) => (
+            <li className="header-li text-white" key={item.id}>
+              <a href={item.navPlace}>{item.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      {toggleNav && (
+        <div className="block md:hidden mobile-nav">
+          <ul className="flex flex-col">
+            {headerList.map((item) => (
+              <li className="border-nav" key={item.id}>
+                {item.name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
 
-            <button onClick={() => setToggleNav(!toggleNav)} className="block md:hidden"> 
-                <Bars3Icon className="h-6"/>
-            </button>
-        </header>
-    )
+      <button
+        onClick={() => setToggleNav(!toggleNav)}
+        className="block md:hidden"
+      >
+        <Bars3Icon className="h-6" />
+      </button>
+    </header>
+  );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,20 +6,29 @@ import { CiTwitter } from "react-icons/ci";
 
 export default function Home() {
   return (
-    <section className="flex flex-col md:flex-row px-10 py-40" id="home">
+    <section
+      className="flex flex-col md:flex-row px-10 py-10 bg-gray-100  min-h-screen items-center"
+      id="home"
+    >
       <div className="w-1/2">
         <h1 className="text-6xl font-hero-font">
-          Hi!, <br /> I'm <span className="text-green-300">Kumaravel</span>
+          Hi!, <br /> I'm{" "}
+          <span className="text-gray-500 home-text">Kumaravel</span>
+          <br />
           <p>I'm a Full stack Developer</p>
         </h1>
         <div className="flex py-5 gap-2">
-          <a href="#" className="hover:text-green-300">
+          <a
+            href="https://www.linkedin.com/in/kumaravel-thondaman"
+            target="_blank"
+            className="hover:text-gray-300 icon-link"
+          >
             <CiLinkedin size={40} />
           </a>
-          <a href="#" className="hover:text-green-300">
+          <a href="#" className="hover:text-gray-300 icon-link">
             <CiInstagram size={40} />
           </a>
-          <a href="#" className="hover:text-green-300">
+          <a href="#" className="hover:text-gray-300 icon-link">
             <CiTwitter size={40} />
           </a>
         </div>
